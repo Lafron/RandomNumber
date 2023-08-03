351fc27c9c4c2233de6aaa2fe23e8e6b7c692550
Epshtein Dmitry <letuver@yahoo.com>
2023-08-03 03:05:26 +0300

    random number

---
+++ script.js
@@ -0,0 +1,43 @@
+'use strict'
+
+function rndBot(rndN) {
+    return function comparNum(clientNum) {
+        rndN == clientNum
+            ? alert("Поздравляю, Вы угадали!!!")
+            :   (
+                rndN > clientNum
+                    ?   (
+                        alert("Загаданное число больше"),
+                        getCheckNum()
+                        )
+                    :   (
+                        alert("Загаданное число меньше"),
+                        getCheckNum()
+                        )
+                );
+    };
+
+};
+
+const comp = rndBot(Math.round(100 * Math.random(), 2));
+
+function getCheckNum() {
+   const answ = prompt("Enter a number");
+
+    if (answ === null) {
+        alert("Игра окончена.");
+    }
+    else if (isNumber(answ)) {
+        comp(answ);
+    } else {
+        alert("Введи число!");
+        getCheckNum();
+    }
+};
+ 
+   
+const isNumber = num => {
+    return !isNaN(parseFloat(num)) && isFinite(num);
+};
+
+getCheckNum();
